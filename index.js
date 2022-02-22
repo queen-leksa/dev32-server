b8d4657dfc676849a64891b71c7186b1f2bef710
Leksa <Surnacheva@ithub.ru>
2022-02-22 17:43:58 +0300

    bugfix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 3002;
 
 const app = express();
 app.use(express.static("./public"));
+app.use(express.json());
 app.use("/api", require("./api/router.js"));
 
 app.listen(PORT);
